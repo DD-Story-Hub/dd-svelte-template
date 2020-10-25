c6d50745a833e01dfa7ffaf2e39dceb24026fb08
Steven Yeo <shihjyun.yeo@gmail.com>
2020-10-25 23:57:02 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Nav from './shared/Nav.svelte'
+  import Footer from './shared/Footer.svelte'
+</script>
+
+<main>
+  <Nav />
+  <article class="main-grid-template">
+    <h1 class="col-start-1 col-end-12 text-green-800 text-center pt-4">
+      DD Story Hub 融數基地
+    </h1>
+    <p class="col-start-3 col-end-10 pt-4">
+      我們是因為喜歡研究數位敘事及資料新聞，而聚集在一起的夥伴，期待能夠透過不同的敘事方式，放大故事影響力。
+      「融數」結合數位（Digital）、數據（Data）的概念，我們希望這個計畫能夠作為同好的聚集地，除了紀錄研究與學習的過程，也將持續分享創新想法。
+    </p>
+  </article>
+  <Footer iconColor="#4e4948" />
+</main>
---
+++ src/shared/Nav.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SocialBox from './SocialBox.svelte';
+</script>
+
+<style>
+  img {
+    width: 220px;
+  }
+</style>
+
+<nav class="nav">
+  <div class="inline-block">
+    <a href="https://medium.com/dd-story-hub/">
+      <figure class="ml-2">
+        <img
+          style="width: 120px"
+          src="assets/DD-regular-logo-text.svg"
+          alt="DD Story Hub logo" />
+      </figure>
+    </a>
+  </div>
+  <div class="inline-block">
+    <SocialBox
+      shareUrl="www.google.com"
+      tnlPageId="139265"
+      iconColor="#4e4948" />
+  </div>
+</nav>
